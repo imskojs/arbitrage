2b8f34bad24560c4d3d07dd3f790bc2cabf7cf94
Seunghoon Ko <imskojs@gmail.com>
2018-05-18 14:20:17 +0900

    setup rollup config

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import nodeResolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import uglify from "rollup-plugin-uglify";
+import { minify } from "uglify-es";
+
+export default {
+  input: './src/index.js',
+  output: {
+    file: './build/index.js',
+    format: 'cjs'
+  },
+  onwarn: (waning) => {
+    console.warn(warning.message);
+  },
+  plugins: [
+    nodeResolve({ jsnext: true, main: true }),
+    commonjs({ include: ['node_modules/rxjs/**', 'node_modules/ramda/**'] }),
+    uglify({}, minify)
+  ]
+};
