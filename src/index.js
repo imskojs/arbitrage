8209c2560ef4af84a394ad97891f9d6153caa261
Seunghoon Ko <imskojs@gmail.com>
2018-05-18 18:43:54 +0900

    remove rollup. rollup really is not for nodejs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import { config as dotEnvConfig } from "dotenv";
 
-dotEnvConfig();;
+import request from "request";
 
-console.log(process.env.BITTREX_API_KEY);
+dotEnvConfig();
 
+const {
+  BITTREX_API_KEY,
+  BITTREX_API_SECRET
+} = process.env
+
+function getMarkets() {
+  // return observable
+  request({
+    method: 'GET',
+    uri: 'https://bittrex.com/api/v1.1/public/getmarkets'
+  }, function (err, res, body) {
+    console.log(body)
+  })
+}
+
+getMarkets()
 
 
---
+++ src/utils/bittrexAPI.js
@@ -0,0 +1,11 @@
+// import * as request from "request";
+
+// export function getMarkets() {
+//   // return observable
+//   request({
+//     method: 'GET',
+//     uri: 'https://bittrex.com/api/v1.1/public/getmarkets'
+//   }, function (err, res, body) {
+//     console.log(body)
+//   })
+// }
